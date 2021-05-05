e656384aba719cb115e281f9e71454d507ba8edd
PaulDenis <d.paul@outlook.it>
2021-05-05 16:29:59 +0200

    Esercizio base completo

--- script/script.js
+++ script/script.js
@@ -1,11 +1,12 @@
 var bombs = [];
 var playerChoise = [];
 var maxBombs = 16;
+var maxRange = 100
 
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 // I numeri non possono essere duplicati.
 do {
-   var number = getRandomNum(1, 100);
+   var number = getRandomNum(1, maxRange);
    if (existInArray(number, bombs) == false) {
       bombs.push(number);
    } 
@@ -16,13 +17,29 @@ console.log(bombs);
 
 // In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L'utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all'utente un altro numero.
 
 var maxAttempts = 100 - bombs.length;
+maxAttempts = 5; //da cancellare
+var gameOver = false;
 
-var 
+do {
+   do {
+      var choise = parseInt(prompt("Inserisci un numero da 1 a " + maxRange));
+   } while (isNaN(choise) || choise < 1 || choise > maxRange);
 
+   if (existInArray(choise, bombs)) {
+      gameOver = true;
+   } else {
+      if (existInArray(choise, playerChoise) == false) {
+         playerChoise.push(choise);
+      }
+   }
+} while (playerChoise.length < maxAttempts && gameOver == false);
+console.log(playerChoise);
 
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all'utente un altro numero.
+console.log("Il tuo punteggio è: " + playerChoise.length);
+
 // La partita termina quando il giocatore inserisce un numero "vietato" o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito.
 
